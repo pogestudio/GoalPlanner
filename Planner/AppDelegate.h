806dc429efb1e05c5011b8b6e7a5bd979744456f
Carl-Arvid Ewerbring <murvid@gmail.com>
2012-09-07 23:04:59 +0200

    Initial Commit

---
+++ Planner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Planner
+//
+//  Created by Carl-Arvid Ewerbring on 9/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
